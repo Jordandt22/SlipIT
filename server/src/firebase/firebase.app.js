d4414b907ef333f68a6221c5fad4417c99d62d18
Jordandt22 <jordandt123411@gmail.com>
2025-06-11 20:12:05 -0700

    (Server): Fixed Firebase for deployment.

--- server/src/firebase/firebase.app.js
+++ server/src/firebase/firebase.app.js
@@ -1,7 +1,13 @@
 const { credential } = require("firebase-admin");
 const { initializeApp } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
-const serviceAccount = require("../../credentials/serviceAccount.json");
+
+let serviceAccount;
+if (process.env.NODE_ENV === "development") {
+  serviceAccount = require("../../credentials/serviceAccount.json");
+} else {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+}
 
 const admin = initializeApp({
   credential: credential.cert(serviceAccount),
