8e478312c9808e6d374e916fc13e8810320582e2
Jordandt22 <jordandt123411@gmail.com>
2025-06-14 01:03:58 -0700

    (Client): Finished Home Page.

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+// Images
+import mainPageBg from "../../../assets/backgrounds/main-page-bg.jpg";
+import mainPageLogo from "../../../assets/logos/main-page-logo.png";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      style={{
+        backgroundImage: `url("${mainPageBg}")`,
+      }}
+      className="home-container center"
+    >
+      <img className="home-container__img" src={mainPageLogo} alt="SlipIT" />
+      <div className="home-container__links center-vertical">
+        <NavLink to="/signup" className="home-container__link-filled">
+          Sign Up
+        </NavLink>
+        <NavLink to="/login" className="home-container__link">
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
