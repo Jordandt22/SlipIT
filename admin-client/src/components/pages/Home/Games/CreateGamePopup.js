9f503aebd3038b123e1547a3da81e1d9e2c043fc
Jordandt22 <jordandt123411@gmail.com>
2025-06-16 17:00:15 -0700

    (Admin-Client): Finished Adding Name Input to Create Game Feature.

--- admin-client/src/components/pages/Home/Games/CreateGamePopup.js
+++ admin-client/src/components/pages/Home/Games/CreateGamePopup.js
@@ -13,11 +13,25 @@ function CreateGamePopup(props) {
   const [showCalendar, setShowCalendar] = useState(false);
   const [addedPlayers, setAddedPlayers] = useState([]);
   const { createGame } = useAPI();
+  const [inputVal, setInputValue] = useState("Game 1");
 
   return (
     <div className="shadow-container center">
       <div className="cg-popup">
         <h2 className="cg-popup__title">Create a Game</h2>
+
+        {/* Name */}
+        <div className="cg-popup__box">
+          <label className="cg-popup__label">Game Name</label>
+          <input
+            type="text"
+            name="name"
+            value={inputVal}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Create a name for this game..."
+            className="cg-popup__input"
+          />
+        </div>
 
         {/* Event Date */}
         <div className="cg-popup__box">
@@ -35,7 +49,6 @@ function CreateGamePopup(props) {
             />
           )}
         </div>
-
         {/* Players */}
         {!showCalendar && (
           <div className="cg-popup__box">
@@ -46,13 +59,16 @@ function CreateGamePopup(props) {
             />
           </div>
         )}
-
         {/* Submit Btn */}
         <button
           type="button"
           className="cg-popup__submit"
           onClick={async () => {
-            await createGame({ eventDate, players: addedPlayers });
+            await createGame({
+              name: inputVal,
+              eventDate,
+              players: addedPlayers,
+            });
             setCreateGamePopup({ show: false });
             refetch();
           }}
